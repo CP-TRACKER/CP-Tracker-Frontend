c11e412bba26dab58a23df8ac396a27751420430
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-07-07 02:34:19 +0530

    reactified carousel

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,26 +1,62 @@
 import React, { Component } from "react";
 
-import Carousel from "nuka-carousel";
+// import Carousel from "nuka-carousel";
+// Install it first!
+
+import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
 
 class CarouselMapped extends Component {
   render() {
     return (
-      <Carousel autoplay={true}>
-        <img
-          src={require("../../assets/cp-carousel.png")}
-          alt="Competitive Programming"
-        />
-        <img
-          src={require("../../assets/python-carousel.png")}
-          alt="Competitive Programming"
-        />
-        <img
-          src={require("../../assets/cpp-carousel.png")}
-          alt="Competitive Programming"
-        />
+      <Carousel
+        wrap={false}
+        pause={false}
+        indicators={false}
+        interval="3000"
+        controls={false}
+      >
+        <Carousel.Item>
+          <Image
+            fluid={true}
+            src={require("../../assets/cp-carousel.png")}
+            alt="Competitive Programming"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            fluid={true}
+            src={require("../../assets/python-carousel.png")}
+            alt="Competitive Programming"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            fluid={true}
+            src={require("../../assets/cpp-carousel.png")}
+            alt="Competitive Programming"
+          />
+        </Carousel.Item>
       </Carousel>
     );
   }
 }
 
 export default CarouselMapped;
+
+/*
+<Carousel autoplay={true}>
+  <img
+    src={require("../../assets/cp-carousel.png")}
+    alt="Competitive Programming"
+  />
+  <img
+    src={require("../../assets/python-carousel.png")}
+    alt="Competitive Programming"
+  />
+  <img
+    src={require("../../assets/cpp-carousel.png")}
+    alt="Competitive Programming"
+  />
+</Carousel>
+*/
